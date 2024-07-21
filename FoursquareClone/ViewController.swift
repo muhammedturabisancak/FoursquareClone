de4ec9b12cc08171e14f00918f82d5f9c2da8abf
Turabi Sancak <muhammedturabisancak@gmail.com>
2024-07-21 23:59:36 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Muhammed Turabi Sancak on 21.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
