1e09e2f511c7543a2a21717eeb5acabb7e41d298
Turabi Sancak <muhammedturabisancak@gmail.com>
2024-07-24 22:17:04 +0300

    Add files via upload

---
+++ DetailsVC.swift
@@ -0,0 +1,32 @@
+//
+//  DetailsVC.swift
+//  FoursquareClone
+//
+//  Created by Muhammed Turabi Sancak on 24.07.2024.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+  
+  
+  @IBOutlet weak var detailsImageView: UIImageView!
+  
+  @IBOutlet weak var detailsNameLabel: UILabel!
+  
+  @IBOutlet weak var detailsTypeLabel: UILabel!
+  
+  @IBOutlet weak var detailsAtmosphereLabel: UILabel!
+  
+  @IBOutlet weak var detailsMapView: MKMapView!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+}
